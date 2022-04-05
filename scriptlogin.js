ba5c780902d9e60d557b4d9e7d121d28669ac5ac
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2022-04-05 19:05:34 -0300

    Add files via upload

--- scriptlogin.js
+++ scriptlogin.js
@@ -25,12 +25,15 @@ function login() {
                     if (pwdcorrect == 1) {
                         console.log('Password correct\nLogged in.');
                         document.getElementById("Message").innerHTML = "Logged in.";
+                        document.getElementById("Message").classList.add('shown');
                     } else {
                         console.log('Password incorrect');
                         document.getElementById("Message").innerHTML = "Credentials are incorrect.";
+                        document.getElementById("Message").classList.add('shown');
                     }
                 } else {
                     document.getElementById("Message").innerHTML = "Credentials are incorrect.";
+                    document.getElementById("Message").classList.add('shown');
                     console.log("User doesn't exist.");
                 }
             }
@@ -38,6 +41,7 @@ function login() {
     } else {
         console.log('Credentials are too short. (4 or more characters required.)');
         document.getElementById("Message").innerHTML = "Credentials are too short. (4 or more characters required.)";
+        document.getElementById("Message").classList.add('shown');
     }
     console.log('\n\n\n\n');
 }
@@ -54,6 +58,7 @@ function addUser() {
                 newusrexists = 1;
                 console.log("User can't be added, as it already exists.")
                 document.getElementById("Message").innerHTML = "User already exists.";
+                document.getElementById("Message").classList.add('shown');
             }
             if (count == BDusr.length) {
                 if (newusrexists == 0) {
@@ -61,12 +66,14 @@ function addUser() {
                     BDpwd.push(newpwd);
                     console.log('User added: ' + BDusr[BDusr.length - 1] + '    Password: ' + BDpwd[BDpwd.length - 1]);
                     document.getElementById("Message").innerHTML = "User added.";
+                    document.getElementById("Message").classList.add('shown');
                 }
             }
         });
     } else {
         console.log('Credentials are too short. (4 or more characters required.)');
         document.getElementById("Message").innerHTML = "Credentials are too short. (4 or more characters required.)";
+        document.getElementById("Message").classList.add('shown');
     }
     console.log('\n\n\n\n');
 }
@@ -89,15 +96,18 @@ function resetPwd() {
                     BDpwd[usrindex] = newpwd;
                     console.log("Password for user: " + BDusr[usrindex] + " changed to: " + BDpwd[usrindex] + '.');
                     document.getElementById("Message").innerHTML = "Password changed.";
+                    document.getElementById("Message").classList.add('shown');
                 } else {
                     console.log("User doesn't exist.");
                     document.getElementById("Message").innerHTML = "User doesn't exist.";
+                    document.getElementById("Message").classList.add('shown');
                 }
             }
         });
     } else {
         console.log('Credentials are too short. (4 or more characters required.)');
         document.getElementById("Message").innerHTML = "Credentials are too short. (4 or more characters required.)";
+        document.getElementById("Message").classList.add('shown');
     }
     console.log('\n\n\n\n');
 }
